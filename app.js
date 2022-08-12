cbcd41cc8fef71611c2fc2f6e683c2339e47812c
Roman Matsypula <ukrmatsypula@gmail.com>
2022-08-12 20:48:06 +0300

    add create new todo and render it

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Globals
 const todoList = document.getElementById("todo-list");
 const userSelect = document.getElementById("user-todo");
+const form = document.querySelector("form");
 let todos = [];
 let users = [];
 
 // Attach Events
 document.addEventListener("DOMContentLoaded", initApp);
+form.addEventListener("submit", handleSubmit);
 
 // basic logic
 function getUserName(userId) {
@@ -57,6 +59,16 @@ function initApp() {
   });
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
+
+  createTodo({
+    userId: Number(form.user.value),
+    title: form.todo.value,
+    completed: false,
+  });
+}
+
 //async
 async function getAllTodos() {
   const response = await fetch("https://jsonplaceholder.typicode.com/todos");
@@ -70,4 +82,17 @@ async function getAllUsers() {
   const data = await response.json();
 
   return data;
+}
+
+async function createTodo(todo) {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
+    method: "POST",
+    body: JSON.stringify(todo),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const newTodo = await response.json();
+
+  printTodo(newTodo);
 }
