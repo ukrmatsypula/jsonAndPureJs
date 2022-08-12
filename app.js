4eac0c934ae056f5e204870c2bb6ce11a05aadf5
Roman Matsypula <ukrmatsypula@gmail.com>
2022-08-12 19:48:33 +0300

    render todos with status and checkbox

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
 // Globals
+const todoList = document.getElementById("todo-list");
 let todos = [];
 let users = [];
 
 // Attach Events
 document.addEventListener("DOMContentLoaded", initApp);
 
+// basic logic
+function getUserName(userId) {
+  const user = users.find((u) => u.id === userId);
 
+  return user.name;
+}
+
+function printTodo({ id, userId, title, completed }) {
+  const li = document.createElement("li");
+  li.classList.add("todo-item");
+  li.dataset.id = id;
+  li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(
+    userId
+  )}</b></span>`;
+
+  const status = document.createElement("input");
+  status.type = "checkbox";
+  status.checked = completed;
+
+  const close = document.createElement("span");
+  close.innerHTML = "&times;";
+  close.classList.add("close");
+
+  li.prepend(status);
+  li.append(close);
+
+  todoList.prepend(li);
+}
 
 // Events logic
 function initApp() {
-  Promise.all([getAllTodos, getAllUsers]).then((values) => {
-    const [todos, users] = values;
+  Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
+    [todos, users] = values;
 
-    console.log(todos, users)
+    console.log(todos, users);
 
     // Отправить в разметку
+    todos.forEach((todo) => printTodo(todo));
   });
 }
 
